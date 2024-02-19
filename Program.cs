01cd0007757c992dc2344de625b632f3eb6b015b
Elias De Windt <edewindt27@outlook.com>
2024-02-18 22:20:41 -0600

    custom middleware added

--- Program.cs
+++ Program.cs
@@ -1,27 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<CustomMiddleware>();
 var app = builder.Build();
 
+app.Use(async (ctx, next) =>
+{
+    await ctx.Response.WriteAsync("You suck ");
+    await next(ctx);
+});
+app.DoSomething();
 app.Run(async (ctx) =>
 {
-    //Status Code
-    ctx.Response.StatusCode = 200;
-    //Response Header
-    ctx.Response.Headers["Key"] = "Value";
-    ctx.Response.Headers["Content-Type"] = "text/html";
-    //Request Properties
-    string path = ctx.Request.Path;
-    string method = ctx.Request.Method;
-    if (ctx.Request.Query.ContainsKey("id"))
-    {
-        string? querystr = ctx.Request.Query["id"];
-        await ctx.Response.WriteAsync($"{querystr}");
-    }
-    string? val = Convert.ToString(ctx.Request.RouteValues["name"]);
-
-    var reader = new StreamReader(ctx.Request.Body);
-    string bod = await reader.ReadToEndAsync();
-    //Final Response
-    await ctx.Response.WriteAsync($"{val}");
+    await ctx.Response.WriteAsync("Third");
 });
-
 app.Run();
